3430472dc2a7c3ff9d095d13ea57352f4ccb57ae
Swedz <email@jonahdunham.com>
2016-11-08 19:58:19 -0500

    Update GuiAPI.java

--- me/Swedz/gapi/GuiAPI.java
+++ me/Swedz/gapi/GuiAPI.java
@@ -5,11 +5,5 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GuiAPI extends JavaPlugin {
-	public static GuiAPI instance;
 	public static InventoryClickEvent event;
-	
-	public void onEnable() {
-		instance = this;
-		Bukkit.getServer().getPluginManager().registerEvents(new ClickEvent(), instance);
-	}
 }
