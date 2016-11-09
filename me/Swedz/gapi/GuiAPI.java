1ded540db64763b7b10aa083baa24ccf65d8c1b3
Swedz <email@jonahdunham.com>
2016-11-08 19:58:31 -0500

    Update GuiAPI.java

--- me/Swedz/gapi/GuiAPI.java
+++ me/Swedz/gapi/GuiAPI.java
@@ -1,9 +1,7 @@
 package me.Swedz.gapi;
 
-import org.bukkit.Bukkit;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.plugin.java.JavaPlugin;
 
-public class GuiAPI extends JavaPlugin {
+public class GuiAPI {
 	public static InventoryClickEvent event;
 }
